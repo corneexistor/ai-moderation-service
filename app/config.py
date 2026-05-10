5eb465524599e3a2152d98ca9dd2a9829f2fc315
corneexistor <existorcorne@gmail.com>
2026-05-10 18:52:11 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,9 +6,9 @@ logger = logging.getLogger(__name__)
 
 @dataclass(frozen=True)
 class ModelConfig:
-    device: str
-    model_size: str
-    use_fp16: bool
+    device: str = "cuda"
+    model_size: str = "medium"
+    use_fp16: bool = False
     in_memory: bool = True
 
 @dataclass(frozen=True)
@@ -19,18 +19,21 @@ class TranscribeConfig:
     # sampling-related options
     temperature: Tuple[float, ...] = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
     best_of: int = 10  # number of independent sample trajectories, if t > 0
-    beam_size: int = 5  # number of beams in beam search
+    beam_size: int = 7  # number of beams in beam search
     patience: float = 2.0  # patience in beam search
 
     # hallucination options
     compression_ratio_threshold: float = 2.6
-    log_prob_threshold: float = -1.5
-    no_speech_threshold: float = 0.6
+    log_prob_threshold: float = -2.0
+    no_speech_threshold: float = 0.8
     hallucination_silence_threshold: float = 2.0
     condition_on_previous_text: bool = False
 
     word_timestamps: bool = True
     append_punctuations: str =  "\"'.。,，!！?？:：)]}、"
+    
+    vad_filter: bool = True
+    condition_on_previous_text: bool = False
 
 
 @dataclass(frozen=True)
